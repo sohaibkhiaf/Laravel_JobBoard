3134de0e0535cc717851a44c629e09ce07375521
Khiaf Sohaib <sohaibkhiaf@gmail.com>
2024-06-15 17:42:23 +0100

    updated my_job_applications.index

--- resources/views/my_job_applications/index.blade.php
+++ resources/views/my_job_applications/index.blade.php
@@ -2,32 +2,38 @@
     <x-breadcrumbs :links="['My Job Applications' => '#']"/>
 
     @forelse ($jobApplications as $application)
-        <x-job-card :jobOffer="$application->jobOffer">
-            <div>
-                <div>
-                    <div>
-                        Applied {{$application->created_at->diffForHumans()}}
+        <x-job-card class="job-application" :jobOffer="$application->jobOffer">
+            <div class="application-container">
+                <div class="application-top">
+
+                    <div class="application-top-applicants">
+                        <div>
+                            Applied {{$application->created_at->diffForHumans()}}
+                        </div>
+    
+                        <div>
+                            Other {{  Str::plural('applicant' , $application->jobOffer->job_applications_count -1)}}
+                            {{$application->jobOffer->job_applications_count -1}}
+                        </div>
+                    </div>
+                    
+                    <div class="application-top-salaries">
+                        <div>
+                            Your asking salary {{number_format($application->expected_salary)}}
+                        </div>
+    
+                        <div>
+                            Average asking salary ${{number_format($application->jobOffer->job_applications_avg_expected_salary)}}
+                        </div>
                     </div>
 
-                    <div>
-                        Other {{  Str::plural('applicant' , $application->jobOffer->job_applications_count -1)}}
-                        {{$application->jobOffer->job_applications_count -1}}
-                    </div>
-
-                    <div>
-                        Your asking salary {{number_format($application->expected_salary)}}
-                    </div>
-
-                    <div>
-                        Average asking salary ${{number_format($application->jobOffer->job_applications_avg_expected_salary)}}
-                    </div>
                 </div>
 
-                <div>
+                <div class="application-bottom">
                     <form action="{{route('myJobApplications.destroy' ,$application)}}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <x-button>Cancel</x-button>
+                        <x-button class="cancel-application-button">Cancel</x-button>
                     </form>
                 </div>
             </div>
