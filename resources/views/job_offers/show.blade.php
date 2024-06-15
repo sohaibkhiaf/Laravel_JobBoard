56f42afec9a3f4581c4864d81f117667a3b475b1
Khiaf Sohaib <sohaibkhiaf@gmail.com>
2024-06-15 17:17:38 +0100

    job_offers.show

--- resources/views/job_offers/show.blade.php
+++ resources/views/job_offers/show.blade.php
@@ -1,43 +1,41 @@
 <x-layout>
     <x-breadcrumbs :links="['Jobs' => route('jobOffers.index') , $jobOffer->title => '#']"/>
-    <x-job-card :$jobOffer> 
+    <x-job-card class="job-offer-details" :$jobOffer>
         <p>
             {!! nl2br( e($jobOffer->description)) !!}
         </p>
 
         @can('apply', $jobOffer)
-            <x-link-button href="{{ route('jobOffer.jobApplication.create' , ['jobOffer' => $jobOffer]) }}">
+            <x-link-button class="apply-button" href="{{ route('jobOffer.jobApplication.create' , ['jobOffer' => $jobOffer]) }}">
                 Apply
             </x-link-button>
         @else
-            <div>You already applied to this job</div>
+            <div>You already applied to this job or you are not logged in yet</div>
         @endcan
 
     </x-job-card>
 
 
-    <x-card>
+    <x-card class="more-jobs">
         <h2>More {{$jobOffer->employer->company_name }} Jobs</h2>
 
         <div>
-            @foreach ($jobOffer->employer->jobOffers as $otherjobOffer)
-                <div style="margin-bottom: 1rem">
-                    <div style="display: flex; justify-content:space-between">
-
-                        <div>
-                            <a href="{{route('jobOffers.show' , ['jobOffer' => $otherjobOffer])}}">
-                                {{$otherjobOffer->title}}
+            @foreach ($jobOffer->employer->jobOffers as $otherJobOffer)
+                <div class="other-job-offer">
+                    <div class="other-job-offer-top">
+                        <div class="job-title">
+                            <a href="{{route('jobOffers.show' , ['jobOffer' => $otherJobOffer])}}">
+                                {{$otherJobOffer->title}}
                             </a>
                         </div>
 
-                        <div>
-                            {{$otherjobOffer->created_at->diffForHumans()}}
+                        <div class="job-created">
+                            {{$otherJobOffer->created_at->diffForHumans()}}
                         </div>
-
                     </div>
 
-                    <div>
-                        ${{number_format($otherjobOffer->salary)}}
+                    <div class="other-job-offer-bottom">
+                        ${{number_format($otherJobOffer->salary)}}
                     </div>
                 </div>
             @endforeach
